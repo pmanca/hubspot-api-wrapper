a0611920fca8c9b200bfe798c07b0128b1142546
Peter Manca <pmanca@hubspot.com>
2017-02-03 10:49:28 -0500

    added second function

--- crm.js
+++ crm.js
@@ -2,6 +2,7 @@ var axios = require('axios')
 var hubCache = require('./init').hubCache
 var contact = require('./contact').contact
 var company = require('./company').company
+var deal = require('./deal').deal
 
 exports.crm = {
 	createContactAndAssociateToCompany: (properties,companyID) => {
@@ -19,5 +20,21 @@ exports.crm = {
 			}).catch(err =>{
 				return err
 			})
+	},
+	createContactAndAssociateToDeal: (properties,dealID) => {
+		
+		return contact.create(properties).then(result => {
+			if(result.data == undefined){
+			    	return {"message": "Failed to create contact",
+			    			"err": result}
+			}
+			return deal.associate(dealID,"CONTACT",[result.data.vid]).then(result => {
+				return result
+			}).catch(err => {
+				return err
+			})
+		}).catch(err => {
+			return err
+		})
 	}
 }
