2aa402a21abb141bdf66b517c16944eaaf885f8c
Peter Manca <pmanca@hubspot.com>
2017-02-05 15:25:42 -0500

    finishing the owner ovject

--- owner.js
+++ owner.js
@@ -19,5 +19,26 @@ exports.owner = {
 				return err
 			})
 		}
+	},
+	getAll: (includeInactive) => {
+		if(!includeInactive){
+			includeInactive = false
+		}
+		var endpoint = `http://api.hubapi.com/owners/v2/owners?includeInactive=` + includeInactive
+		if(hubCache.get("config").type == "hapikey"){
+			endpoint += `&hapikey=` + hubCache.get("config").value			
+			return axios.get(endpoint).then(response => {
+				return response
+			})
+		}else{
+			var token = hubCache.get("config").value
+			return axios.get(endpoint,
+				{headers: {"Authorization": "Bearer " + token }
+			}).then(response => {
+				return response
+			}).catch(err =>{
+				return err
+			})
+		}
 	}
 }
